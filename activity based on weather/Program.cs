549b2e8f68cb15bbc7193493f1ad85bd63fac9a8
Kristen Shea <kstar61@live.com>
2018-09-03 17:22:27 -0500

    Add project files.

---
+++ activity based on weather/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace activity_based_on_weather
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
